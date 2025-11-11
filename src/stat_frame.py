fce2a6890808d296ab8a47540808f967a0d592a1
riz3ndrr <riz3ndrr@gmail.com>
2025-11-11 23:40:46 +1300

    general touchups

--- src/stat_frame.py
+++ src/stat_frame.py
@@ -15,23 +15,37 @@ class StatFrame():
         self.event = event
         self.header.place(relx = 0.2, rely = 0.05)
         self.switch_frame_button = customtkinter.CTkButton(master = self.frame, text = "Main Menu",
-                                                           command = lambda: switch_frame_func(START))
-        self.switch_frame_button.place(relx = 0.8, rely = 0.9)
+                                                           command = lambda: switch_frame_func(START),
+                                                           width = 200, height = 50,
+                                                           font = ("TkDefaultFont", 20))
+        self.switch_frame_button.place(relx = 0.75, rely = 0.95)
 
         filename = DIR1 + event + DIR2
-        self.data_frame = pd.read_csv(filename)
+        try:
+            self.data_frame = pd.read_csv(filename)
+        except FileNotFoundError:
+            default_data = [
+                ["t1", "t2", "t3", "t4", "t5", "average", "placing", "num_ppl"],
+                [1.1, 1.2, 1.3, 1.4, 1.5, None, 1, 5],
+                [2, 2, 2, 2, 2, 2, 2, 2]
+            ]
+            with open(filename, 'w', newline='') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerows(default_data)
+            self.data_frame = pd.read_csv(filename)
 
-        self.results_header = customtkinter.CTkLabel(self.frame, text = "Recent 50 Times:",
-                                                     font = ("TkDefaultFont", 30))
-        self.results_header.place(relx = 0.1, rely = 0.425)
+        self.showGraph()
+        
+
         self.top_result_container = customtkinter.CTkFrame(self.frame, width = 300, height = 500,
                                                            fg_color = "transparent")
+        self.top_result_container.place(relx = 0.6, rely = 0.4)
 
         self.showSummaryStats() 
 
         ## DISPLAY TIMES 
         self.displayTopResults()
-        self.showGraph()
+        
     def genSuffix(self, number):
         conversion = {0 : "th",
                       1 : "st",
@@ -71,8 +85,8 @@ class StatFrame():
         self.mean_time_label.place(relx = 0.125 + 0.30, rely = 0.175)
         
 
-        mean_avg = results["average"].tail(10).mean() 
-        self.mean_avg_header = customtkinter.CTkLabel(self.frame, text = "Mo10Ao5", font = ("TkDefaultFont", 30))
+        mean_avg = results["average"].tail(10).mean()
+        self.mean_avg_header = customtkinter.CTkLabel(self.frame, text = f"Mo{min(10, len(results))}Ao5", font = ("TkDefaultFont", 30))
         self.mean_avg_label = customtkinter.CTkLabel(self.frame, text = f"{mean_avg:.2f}s", font = ("TkDefaultFont", 25))
         self.mean_avg_header.place(relx = 0.1 + 0.55, rely = 0.125)
         self.mean_avg_label.place(relx = 0.125 + 0.55, rely = 0.175)
@@ -83,7 +97,8 @@ class StatFrame():
         results = self.data_frame.iloc[:-2]
         time_cols = ["t1", "t2", "t3", "t4", "t5"]
         times = results[time_cols].melt()
-        times = times.tail(50)
+        NUM_TIMES_TO_DISPLAY = 50
+        times = times.tail(NUM_TIMES_TO_DISPLAY)
 
         fig = Figure(figsize = (5,4), dpi = 100) 
         ax = fig.add_subplot(111)
@@ -93,9 +108,14 @@ class StatFrame():
         times.plot(ax = ax, legend=False)
         canvas = FigureCanvasTkAgg(fig, master = self.frame)
         canvas.draw() 
-        canvas.get_tk_widget().place(relx = 0.1, rely = 0.5)
+        canvas.get_tk_widget().place(relx = 0.1, rely = 0.4)
 
         times.plot.line()
+
+        num_times = min(NUM_TIMES_TO_DISPLAY, len(times))
+        self.results_header = customtkinter.CTkLabel(self.frame, text = f"Recent {num_times} Times:",
+                                                     font = ("TkDefaultFont", 30))
+        self.results_header.place(relx = 0.15, rely = 0.375)
 
 
 
@@ -129,7 +149,7 @@ class StatFrame():
                                            anchor = "w", justify = "left", width = 130) 
             label.pack() 
 
-        self.top_result_container.place(relx = 0.65, rely = 0.5)
+        
         
         
     def toggleSinAvg(self):
